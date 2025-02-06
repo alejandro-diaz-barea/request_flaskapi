e20010c8e6295eb97e9408166c2179b6c0ead866
alejandro-diaz-barea <126179563+alejandro-diaz-barea@users.noreply.github.com>
2025-02-06 13:28:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ async def options_login():
     return JSONResponse(
         content={"message": "Preflight request successful"},
         headers={
-            "Access-Control-Allow-Origin": "https://deploy-react-front-g4.vercel.app",  # Cambia esto según el origen exacto
+            "Access-Control-Allow-Origin": "https://deploy-reactfront.onrender.com",  # Cambia esto según el origen exacto
             "Access-Control-Allow-Methods": "POST, OPTIONS",
             "Access-Control-Allow-Headers": "Authorization, Content-Type",
             "Access-Control-Allow-Credentials": "true",
